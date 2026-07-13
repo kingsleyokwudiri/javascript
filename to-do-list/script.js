e5b22e1a5c270924f6997594ce9bd498c27f4f30
Kingsley <kingsleyokwudiri101@gmail.com>
2026-07-13 14:30:25 +0100

    Refactor todo list rendering and update addTodo function to push object structure

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -8,25 +8,34 @@ let todoList = [
     dueDate: "2022-12-22",
   },
 ];
+
 function renderTodoList() {
   let todoListHTML = "";
   for (let i = 0; i < todoList.length; i++) {
     const todoObject = todoList[i];
+    // const { name, dueDate } = todoObject; // Destructuring assignment (optional)
     const name = todoObject.name;
     const dueDate = todoObject.dueDate;
     const html = `<p>${name} ${dueDate} <button onclick="todoList.splice(${i}, 1); renderTodoList();">Delete</button></p>`;
     todoListHTML += html;
-    document.querySelector(".js-todo-list").innerHTML = todoListHTML;
   }
+  // Moved outside the loop so it clears correctly when the list is empty
+  document.querySelector(".js-todo-list").innerHTML = todoListHTML;
 }
 
 function addTodo() {
   const inputElement = document.querySelector(".js-name-input");
   const name = inputElement.value;
+  const dateInputElement = document.querySelector(".js-date-input");
+
   if (name === "") {
     alert("Please enter a task!");
   } else {
-    todoList.push(name);
+    // Fixed: Now pushes an object structure to match the initial list array
+    todoList.push({
+      name: name,
+      dueDate: "", // If you add a date selector later, grab its value here
+    });
   }
 
   inputElement.value = "";
@@ -38,3 +47,6 @@ function calculationkeyDown(event) {
     addTodo();
   }
 }
+
+// Initial render to display the default tasks
+renderTodoList();
