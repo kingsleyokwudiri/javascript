7b68a764e7b71df8eab638cb9f0fde3e5236992c
Kingsley <kingsleyokwudiri101@gmail.com>
2026-07-16 22:09:06 +0100

    added an autoplay button

---
+++ Basics/advanced-functions.js
@@ -0,0 +1,45 @@
+/*function greeting() {
+  console.log("hello");
+}
+greeting();
+const num = 2;
+// function1 contains a function
+const function1 = function () {
+  // anonymous function
+  console.log("hello");
+};
+
+function1(); // cannot be accessed before initialization (cannot be hoisted)
+console.log(function1);
+// a function is just another type of value
+console.log(typeof function1);
+// a method is a function that's saved in an object
+
+const object1 = {
+  num: 2,
+  fun: function greeting() {
+    console.log("hello");
+  },
+};
+// using dot notation to call the function
+object1.fun();
+// passing a function into another function
+function run(param) {
+  param();
+}
+run(function () {
+  console.log("hello");
+});
+*/
+// activates a function after a set time in milliseconds
+// 1000ms = 1 second
+// only activates once
+setTimeout(function () {
+  console.log("timeout");
+}, 7000);
+console.log("next line");
+// activates a function after a set time
+// activates more than once
+setInterval(function () {
+  console.log("interval");
+}, 3000);
--- Mini_Projects/rockpaperscissors/script.js
+++ Mini_Projects/rockpaperscissors/script.js
@@ -4,7 +4,6 @@ let score = JSON.parse(localStorage.getItem("score")) || {
   losses: 0,
   ties: 0,
 };
-
 function updateScoreElement() {
   document.querySelector(".js-score").innerHTML =
     `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
@@ -79,7 +78,7 @@ function playGame(playerMove) {
   }
 
   localStorage.setItem("score", JSON.stringify(score));
-  
+
   updateResultDisplay(result);
   updateScoreElement();
 
