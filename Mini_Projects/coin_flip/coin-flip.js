b356aba8b8fb22dd54d9e64657876aed5eb23667
Kingsley <kingsleyokwudiri101@gmail.com>
2026-07-18 22:35:54 +0100

    fixed the coin flip game and cleaned up the code

--- Mini_Projects/coin_flip/coin-flip.js
+++ Mini_Projects/coin_flip/coin-flip.js
@@ -1,38 +1,79 @@
-let randomNumber;
-let guess = 'heads';
+// Initialize score with default variables if it doesn't exist.
+// JSON.parse converts the string back to an object
+let score = JSON.parse(localStorage.getItem("score")) || {
+  wins: 0,
+  losses: 0,
+  ties: 0,
+};
+let guess;
+let choice;
 let result;
+let randomNumber = Math.random();
 function chance() {
   let randomNumber = Math.random();
-  let result = randomNumber;
   if (randomNumber < 0.5) {
-    result = 'heads';
-    console.log('less than');
+    result = "heads";
   } else if (randomNumber > 0.5) {
-    result = 'tails';
-    console.log('bigger than');
-
+    result = "tails";
   } else {
-    alert('tie');
+    alert("tie");
+    score.ties += 1;
   }
 }
-function playGame() {
-  chance();
-  result = "";
-  if (guess === result) {
-    console.log('You win');
-  } else if (!(guess === result)) {
-    console.log('You lose');
+
+function coinSide(side) {
+  if (side === "heads") {
+    choice = "heads";
+  } else if (side === "tails") {
+    choice = "tails";
   }
-} 
-playGame();
-console.log(randomNumber);
+}
+function flipCoin() {
+  chance();
+  if (choice != "heads" && choice != "tails") {
+    alert("Please pick a choice!");
+  } else {
+    if (choice === "heads" && result === "heads") {
+      score.wins += 1;
+      alert(
+        `You win! \nYou picked ${choice}, Coin showed ${result}.\nWins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`,
+      );
+    } else if (choice === "heads" && result === "tails") {
+      score.losses += 1;
+      alert(
+        `You lose! \nYou picked ${choice}, Coin showed ${result}.\n Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`,
+      );
+    } else if (choice === "tails" && result === "tails") {
+      score.wins += 1;
+      alert(
+        `You win! \nYou picked ${choice}, Coin showed ${result}.\n Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`,
+      );
+    } else if (choice === "tails" && result === "heads") {
+      score.losses += 1;
 
-
-
-
-
-
-
-
-
-
+      // localStorage only supports strings, use JSON.stringify() to convert
+      localStorage.setItem("score", JSON.stringify(score));
+      alert(
+        `You lose! \nYou picked ${choice}, Coin showed ${result}.\n Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`,
+      );
+    }
+  }
+}
+function resetScores() {
+  score.wins = 0;
+  score.losses = 0;
+  score.ties = 0;
+  alert(
+    `Scores have been reset!\n Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`,
+  );
+  localStorage.setItem("score", JSON.stringify(score));
+}
+function showScores() {
+  alert(`Wins: ${score.wins}\nLosses: ${score.losses} \nTies: ${score.ties}`);
+}
+function showSide() {
+  coinSide();
+  if (choice === undefined) {
+    alert("Please pick a choice!");
+  } else alert(`Your Choice: ${choice}`);
+}
