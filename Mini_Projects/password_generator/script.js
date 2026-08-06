424f34553c8203cfc95b5bba10b0401abfce0e42
Kingsley <kingsleyokwudiri101@gmail.com>
2026-08-06 14:34:47 +0100

    updated the password generator

--- Mini_Projects/password_generator/script.js
+++ Mini_Projects/password_generator/script.js
@@ -6,8 +6,9 @@ const number = "0123456789";
 const symbols = "@#$%^&*()_+~|}{[]></-=";
 const allChars = upperCase + lowerCase + number + symbols;
 
+let password = "";
+
 function generatePassword() {
-  let password = "";
   password += upperCase[Math.floor(Math.random() * upperCase.length)];
   password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
   password += number[Math.floor(Math.random() * number.length)];
@@ -18,6 +19,64 @@ function generatePassword() {
   passwordBox.value = password;
 }
 
+let intervalId = null;
+const message = document.getElementById("password-message");
+const copiedMessage = document.getElementById("copy");
+
+// for copying the password
+function copyPassword() {
+  passwordBox.select();
+  document.execCommand("copy");
+}
+
+function clearConfirmation() {
+  if (intervalId !== null) {
+    clearTimeout(intervalId);
+    intervalId = null;
+  }
+  message.textContent = "";
+}
+
+function showConfirmation() {
+  clearConfirmation();
+  message.textContent = "Password has been generated.";
+  intervalId = setTimeout(function () {
+    message.textContent = "";
+    intervalId = null;
+  }, 2000);
+}
+
+function showCopiedMessage() {
+  if (!password) {
+    alert("No password generated.");
+  } else {
+    clearCopiedMessage();
+    const originalText = copiedMessage.textContent;
+    copiedMessage.textContent = "Copied!";
+    intervalId = setTimeout(function () {
+      copiedMessage.textContent = originalText;
+      intervalId = null;
+    }, 1000);
+  }
+}
+
+function clearCopiedMessage() {
+  if (intervalId !== null) {
+    clearTimeout(intervalId);
+    intervalId = null;
+  }
+  copiedMessage.textContent = "Copy";
+}
+
 document.querySelector(".password-button").addEventListener("click", () => {
   generatePassword();
+});
+
+document.querySelector(".copy-button").addEventListener("click", () => {
+  copyPassword();
+  showCopiedMessage();
+});
+
+document.querySelector(".password-button").addEventListener("click", () => {
+  showConfirmation();
 });
