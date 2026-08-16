f9bee1a3fab1cac62c2480b87f92716bc4d45e84
Kingsley <kingsleyokwudiri101@gmail.com>
2026-08-17 00:48:26 +0100

    Create class version of cart

---
+++ Mini_Projects/amazon/data/cart-class.js
@@ -0,0 +1,97 @@
+class Cart {
+  cartItems;
+  localStorageKey;
+  // constructor should not return anything
+  constructor(localStorageKey) {
+    this.localStorageKey = localStorageKey;
+    this.loadFromStorage();
+  }
+
+  loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+
+    if (!this.cartItems) {
+      this.cartItems = [
+        {
+          productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+          quantity: 2,
+          deliveryOptionId: "1",
+        },
+        {
+          productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+          quantity: 1,
+          deliveryOptionId: "2",
+        },
+      ]; // Empty cart by default
+    }
+  }
+  saveToStorage() {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+  }
+  addToCart(productId, quantity) {
+    let matchingItem;
+
+    this.cartItems.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += quantity;
+    } else {
+      this.cartItems.push({
+        productId: productId,
+        quantity: quantity,
+        deliveryOptionId: "1",
+      });
+    }
+    this.saveToStorage();
+  }
+  removeFromCart(productId) {
+    const newCart = [];
+    this.cartItems.forEach((cartItem) => {
+      if (cartItem.productId !== productId) {
+        newCart.push(cartItem);
+      }
+    });
+    this.cartItems = newCart;
+    this.saveToStorage();
+  }
+  updateDeliveryOption(productId, deliveryOptionId) {
+    let matchingItem;
+    this.cartItems.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+    matchingItem.deliveryOptionId = deliveryOptionId;
+    this.saveToStorage();
+  }
+  updateQuantity(productId, newQuantity) {
+    // Validation: check if newQuantity is >= 0 and < 1000
+    if (newQuantity < 0 || newQuantity >= 1000) {
+      console.log("Quantity must be between 0 and 999");
+      return false;
+    }
+
+    let found = false;
+    this.cartItems.forEach((cartItem) => {
+      if (cartItem.productId === productId) {
+        cartItem.quantity = newQuantity;
+        found = true;
+      }
+    });
+    if (!found) {
+      console.log("Product not found in cart");
+      return false;
+    }
+    this.saveToStorage();
+    return true;
+  }
+}
+// generates a new object
+const cart = new Cart("cart-oop");
+const businessCart = new Cart("cart-business");
+
+// console.log(businessCart instanceof Cart);
--- Mini_Projects/amazon/scripts/checkout.js
+++ Mini_Projects/amazon/scripts/checkout.js
@@ -1,7 +1,7 @@
 import { cart } from "../data/cart.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import "../data/cart-oop.js";
+import "../data/cart-class.js";
 
 // Call the function to render the order summary
 renderOrderSummary();
