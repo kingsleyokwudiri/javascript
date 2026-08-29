815483a7076e761082bd10eb45544f7454a659c4
Kingsley <kingsleyokwudiri101@gmail.com>
2026-08-29 02:35:50 +0100

    Load products from backend

--- Mini_Projects/amazon/scripts/checkout.js
+++ Mini_Projects/amazon/scripts/checkout.js
@@ -1,9 +1,12 @@
 import { cart } from "../data/cart.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import { loadProducts } from "../data/products.js";
 // import "../data/cart-class.js";
-import "../data/backend-practice.js";
+// import "../data/backend-practice.js";
 
-// Call the function to render the order summary
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts(() => {
+  // Call the function to render the order summary
+  renderOrderSummary();
+  renderPaymentSummary();
+});
